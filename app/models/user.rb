395ad4d05f5e3c0ef2bb2cb77812d02434b6b985
Katie <katie.holden5@gmail.com>
2012-11-03 11:18:46 -0400

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+  acts_as_predecessor
+
+  # attr_accessible :login, :password, :password_confirmation
+  # has_secure_password
+
+  # before_save { |user| user.login = login.downcase }
+  # before_save :create_remember_token
+
+  # validates :login, :presence => true, :uniqueness => { :case_sensitive => false }
+  # validates :password, :presence => true, :length => { :minimum => 6 }
+  # validates :password_confirmation, :presence => true
+
+  # private
+
+  #   def create_remember_token
+  #     self.remember_token = SecureRandom.urlsafe_base64
+  #   end
+
+
+end
