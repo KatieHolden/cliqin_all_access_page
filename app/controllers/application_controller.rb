59d8df906651fd6721fb0608a8ed0b133db05fc1
Katie <katie.holden5@gmail.com>
2012-11-04 01:40:05 -0500

    Trying to fix IP verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,4 @@ class ApplicationController < ActionController::Base
 		protect_from_forgery
 		include SessionsHelper
 
-		before_filter :set_current_ip
-
-
-		  protected
-			def set_current_ip
-				Instructor.IP = request.env['action_dispatch.remote_ip']
-				
-			end
-
 end
