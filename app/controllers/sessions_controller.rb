c08eb3254d41821473dc638c422fefe7194802dd
Katie <katie.holden5@gmail.com>
2012-11-11 12:46:27 -0500

    Add page for instructors to input correct answer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,6 @@ class SessionsController < ApplicationController
   	user = User.find_by_login(params[:session][:login].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      #if user.heir.is_a?(Instructor)
-        #Instructor.update(user.heir.id, :IP => request.env['action_dispatch.remote_ip'])
-      #end
       redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
@@ -19,6 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    #update on course and reset instructor IP
   	sign_out
     redirect_to root_url
   end
