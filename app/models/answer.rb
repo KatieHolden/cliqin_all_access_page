5dcd9dbdbde65eef0e6002b27aa95148597d383f
Jennifer O'Neil <oneilj@67-194-197-5.wireless.umnet.umich.edu>
2012-11-17 23:19:21 -0500

    fixed course launch button and student adding courses

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -13,4 +13,6 @@
 
 class Answer < ActiveRecord::Base
   attr_accessible :answers, :class_date, :course_ID, :total_points
+
+  
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -13,4 +13,26 @@ class Course < ActiveRecord::Base
   attr_accessible :course_name, :instructor_ID, :active
 
   validates :course_name, :presence => true
+
+  # checks to see is a student is has previously enrolled in a course
+  def is_enrolled?(student_id, course_name)
+  	student_in_courses = StudentInCourse.where(:student_id => student_id)
+  	student_in_courses.each do |c|
+  		if c.course_name == course_name
+  			return true
+  		end
+  	end
+  	return false
+  end
+
+  def answer_set_for_today?(course_id)
+  	answer_set = Answer.where(:course_id => course_id)
+
+  	answer_set.each do |a|
+  		if a.class_date == Date.today
+  			return true
+  		end
+  	end
+  	return false
+  end
 end
