8c543fc592532994ce103e64fce5a1ac037de32b
Katie <katie.holden5@gmail.com>
2012-11-24 18:46:20 -0500

    Display error message when a student has already answered the question set for that course and date. This is displayed in place of the Participate in this Course button

--- app/models/student_in_course.rb
+++ app/models/student_in_course.rb
@@ -1,3 +1,13 @@
 class StudentInCourse < ActiveRecord::Base
   attr_accessible :course_ID, :student_ID, :course_name
+
+  def participated_today?(course_ID, student_ID)
+  	grades = Grade.where(:course_ID => course_ID, :student_ID => student_ID, :class_date => Date.today)
+
+  	grades.each do |g|
+  		return true;
+  	end
+  	return false
+  end
+  
 end
